2ecfa7549fb312f9e59aeb902d74753d20ede8b4
KrASSaB4iK <KrASSaB4iK@users.noreply.github.com>
2023-05-22 21:26:16 +0300

    Delete some bugs

--- app/src/main/java/com/example/footballapp/profileActivity.kt
+++ app/src/main/java/com/example/footballapp/profileActivity.kt
@@ -12,8 +12,8 @@ class profileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
     }
     fun toastMe(view: View) {
-        val infoOpenIntent = Intent(this, NickActivity::class.java)
-        startActivity(infoOpenIntent)
+        val myToast = Toast.makeText(this, "Ведется доработка", Toast.LENGTH_SHORT)
+        myToast.show()
     }
     fun infoOpen1(view: View) {
         val infoOpenIntent = Intent(this, NickActivity::class.java)
